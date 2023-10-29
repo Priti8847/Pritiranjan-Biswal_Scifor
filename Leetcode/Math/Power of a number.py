34ee0d076aa5c47d28db365b05d08e929935f394
Priti8847 <121750207+Priti8847@users.noreply.github.com>
2023-10-30 00:11:56 +0530

    Create Power of a number.py

---
+++ Leetcode/Math/Power of a number.py
@@ -0,0 +1,5 @@
+##https://leetcode.com/problems/powx-n/description/?envType=study-plan-v2&envId=programming-skills
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
