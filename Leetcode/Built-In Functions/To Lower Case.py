85b63e347666948954d41333f59162956a603c8e
Priti8847 <121750207+Priti8847@users.noreply.github.com>
2023-10-29 23:57:08 +0530

    Create To Lower Case.py

---
+++ Leetcode/Built-In Functions/To Lower Case.py
@@ -0,0 +1,4 @@
+##https://leetcode.com/problems/to-lower-case/description/?envType=study-plan-v2&envId=programming-skills
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
